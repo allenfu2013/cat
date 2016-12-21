a5d9fe0d4585c37222fa5078456e137fa75f213d
fuyong <fuyong@niwodai.net>
2016-12-21 13:47:36 +0800

    增加报警服务器发送邮件功能

--- cat-home/src/main/java/com/dianping/cat/report/alert/sender/decorator/ExceptionDecorator.java
+++ cat-home/src/main/java/com/dianping/cat/report/alert/sender/decorator/ExceptionDecorator.java
@@ -1,10 +1,11 @@
 package com.dianping.cat.report.alert.sender.decorator;
 
-import java.io.StringWriter;
+import java.io.*;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 import org.codehaus.plexus.personality.plexus.lifecycle.phase.Initializable;
 import org.codehaus.plexus.personality.plexus.lifecycle.phase.InitializationException;
@@ -20,78 +21,102 @@ import freemarker.template.Template;
 
 public class ExceptionDecorator extends ProjectDecorator implements Initializable {
 
-	@Inject
-	private AlertSummaryExecutor m_executor;
+    @Inject
+    private AlertSummaryExecutor m_executor;
 
-	public Configuration m_configuration;
+    public Configuration m_configuration;
 
-	public static final String ID = AlertType.Exception.getName();
+    public static final String ID = AlertType.Exception.getName();
 
-	protected DateFormat m_linkFormat = new SimpleDateFormat("yyyyMMddHH");
+    protected DateFormat m_linkFormat = new SimpleDateFormat("yyyyMMddHH");
 
-	@Override
-	public String generateContent(AlertEntity alert) {
-		Map<Object, Object> dataMap = generateExceptionMap(alert);
-		StringWriter sw = new StringWriter(5000);
+    private static final String PROPERTIES_ALERT = "/data/appdatas/cat/cat.properties";
+    private Properties catProps = new Properties();
 
-		try {
-			Template t = m_configuration.getTemplate("exceptionAlert.ftl");
-			t.process(dataMap, sw);
-		} catch (Exception e) {
-			Cat.logError("build exception content error:" + alert.toString(), e);
-		}
+    @Override
+    public String generateContent(AlertEntity alert) {
+        Map<Object, Object> dataMap = generateExceptionMap(alert);
+        StringWriter sw = new StringWriter(5000);
 
-		String alertContent = sw.toString();
-		String summaryContext = "";
+        try {
+            Template t = m_configuration.getTemplate("exceptionAlert.ftl");
+            t.process(dataMap, sw);
+        } catch (Exception e) {
+            Cat.logError("build exception content error:" + alert.toString(), e);
+        }
 
-		try {
-			summaryContext = m_executor.execute(alert.getGroup(), alert.getDate());
-		} catch (Exception e) {
-			Cat.logError(alert.toString(), e);
-		}
+        String alertContent = sw.toString();
+        String summaryContext = "";
 
-		if (summaryContext != null) {
-			return alertContent + "<br/>" + summaryContext;
-		} else {
-			return alertContent;
-		}
-	}
+        try {
+            summaryContext = m_executor.execute(alert.getGroup(), alert.getDate());
+        } catch (Exception e) {
+            Cat.logError(alert.toString(), e);
+        }
 
-	protected Map<Object, Object> generateExceptionMap(AlertEntity alert) {
-		String domain = alert.getGroup();
-		String contactInfo = buildContactInfo(domain);
-		Map<Object, Object> map = new HashMap<Object, Object>();
+        if (summaryContext != null) {
+            return alertContent + "<br/>" + summaryContext;
+        } else {
+            return alertContent;
+        }
+    }
 
-		map.put("domain", domain);
-		map.put("content", alert.getContent());
-		map.put("date", m_format.format(alert.getDate()));
-		map.put("linkDate", m_linkFormat.format(alert.getDate()));
-		map.put("contactInfo", contactInfo);
+    protected Map<Object, Object> generateExceptionMap(AlertEntity alert) {
+        String domain = alert.getGroup();
+        String contactInfo = buildContactInfo(domain);
+        Map<Object, Object> map = new HashMap<Object, Object>();
 
-		return map;
-	}
+        map.put("domain", domain);
+        map.put("content", alert.getContent());
+        map.put("date", m_format.format(alert.getDate()));
+        map.put("linkDate", m_linkFormat.format(alert.getDate()));
+        map.put("contactInfo", contactInfo);
+        map.put("catHost", catProps.getProperty("cat.host"));
+        System.out.println("######## " + map);
+        return map;
+    }
 
-	@Override
-	public String generateTitle(AlertEntity alert) {
-		StringBuilder sb = new StringBuilder();
-		sb.append("[CAT异常告警] [项目: ").append(alert.getGroup()).append("]");
-		return sb.toString();
-	}
+    @Override
+    public String generateTitle(AlertEntity alert) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[CAT异常告警] [项目: ").append(alert.getGroup()).append("]");
+        return sb.toString();
+    }
 
-	@Override
-	public String getId() {
-		return ID;
-	}
+    @Override
+    public String getId() {
+        return ID;
+    }
 
-	@Override
-	public void initialize() throws InitializationException {
-		m_configuration = new Configuration();
-		m_configuration.setDefaultEncoding("UTF-8");
-		try {
-			m_configuration.setClassForTemplateLoading(this.getClass(), "/freemaker");
-		} catch (Exception e) {
-			Cat.logError(e);
-		}
-	}
+    @Override
+    public void initialize() throws InitializationException {
+        m_configuration = new Configuration();
+        m_configuration.setDefaultEncoding("UTF-8");
+        InputStream in = null;
+        InputStreamReader reader = null;
+        try {
+            in = new FileInputStream(new File(PROPERTIES_ALERT));
+            reader = new InputStreamReader(in, "UTF-8");
+            catProps.load(reader);
+            m_configuration.setClassForTemplateLoading(this.getClass(), "/freemaker");
+        } catch (Exception e) {
+            Cat.logError(e);
+        } finally {
+            if (null != in) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    Cat.logError(e);
+                }
+            }
+            if (null != reader) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    Cat.logError(e);
+                }
+            }
+        }
+    }
 
 }
--- cat-home/src/main/java/com/dianping/cat/report/alert/sender/sender/MailSender.java
+++ cat-home/src/main/java/com/dianping/cat/report/alert/sender/sender/MailSender.java
@@ -1,46 +1,85 @@
 package com.dianping.cat.report.alert.sender.sender;
 
+import java.io.*;
 import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Properties;
 
 import com.dianping.cat.Cat;
 import com.dianping.cat.report.alert.sender.AlertChannel;
 import com.dianping.cat.report.alert.sender.AlertMessageEntity;
+import org.apache.commons.mail.HtmlEmail;
+import org.codehaus.plexus.personality.plexus.lifecycle.phase.Initializable;
+import org.codehaus.plexus.personality.plexus.lifecycle.phase.InitializationException;
 
-public class MailSender extends AbstractSender {
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
 
-	public static final String ID = AlertChannel.MAIL.getName();
+public class MailSender extends AbstractSender implements Initializable {
 
-	@Override
-	public String getId() {
-		return ID;
-	}
+    public static final String ID = AlertChannel.MAIL.getName();
 
-	@Override
-	public boolean send(AlertMessageEntity message) {
-		com.dianping.cat.home.sender.entity.Sender sender = querySender();
-		boolean batchSend = sender.isBatchSend();
-		boolean result = false;
+    private static final String PROPERTIES_ALERT = "/data/appdatas/cat/cat.properties";
 
-		if (batchSend) {
-			String emails = message.getReceiverString();
+    private boolean initialized = false;
 
-			result = sendEmail(message, emails, sender);
-		} else {
-			List<String> emails = message.getReceivers();
+    private final Properties catProps = new Properties();
 
-			for (String email : emails) {
-				boolean success = sendEmail(message, email, sender);
-				result = result || success;
-			}
-		}
-		return result;
-	}
+    Authenticator authenticator = null;
 
-	private boolean sendEmail(AlertMessageEntity message, String receiver,
-	      com.dianping.cat.home.sender.entity.Sender sender) {
+    @Override
+    public String getId() {
+        return ID;
+    }
+
+    /*@Override
+    public boolean send(AlertMessageEntity message) {
+        com.dianping.cat.home.sender.entity.Sender sender = querySender();
+        boolean batchSend = sender.isBatchSend();
+        boolean result = false;
+
+        if (batchSend) {
+            String emails = message.getReceiverString();
+
+            result = sendEmail(message, emails, sender);
+        } else {
+            List<String> emails = message.getReceivers();
+
+            for (String email : emails) {
+                boolean success = sendEmail(message, email, sender);
+                result = result || success;
+            }
+        }
+        return result;
+    }*/
+
+    @Override
+    public boolean send(AlertMessageEntity message) {
+        if (!initialized) return false;
+        m_logger.info(String.format("start to send email alert message, title:%s, receivers:%s", message.getTitle(), message.getReceiverString()));
+        com.dianping.cat.home.sender.entity.Sender sender = querySender();
+        if (sender == null) {
+            return false;
+        }
+        boolean batchSend = sender.isBatchSend();
+        boolean result = false;
+        if (batchSend) {
+            String emails = message.getReceiverString();
+            result = sendEmail(message, emails);
+        } else {
+            List<String> emails = message.getReceivers();
+            for (String email : emails) {
+                boolean success = sendEmail(message, email);
+                result = result || success;
+            }
+        }
+        return result;
+    }
+
+	/*private boolean sendEmail(AlertMessageEntity message, String receiver,
+          com.dianping.cat.home.sender.entity.Sender sender) {
 		String title = message.getTitle().replaceAll(",", " ");
 		String content = message.getContent().replaceAll(",", " ");
 		String urlPrefix = sender.getUrl();
@@ -57,5 +96,64 @@ public class MailSender extends AbstractSender {
 		}
 
 		return httpSend(sender.getSuccessCode(), sender.getType(), urlPrefix, urlPars);
-	}
+	}*/
+
+    private boolean sendEmail(AlertMessageEntity alertEntity, String receiver) {
+        try {
+            HtmlEmail email = new HtmlEmail();
+            email.setHostName(catProps.getProperty("mail.smtp.host"));
+            email.setAuthentication(catProps.getProperty("mail.user"), catProps.getProperty("mail.password"));
+            email.setCharset("UTF-8");
+            email.setFrom(catProps.getProperty("mail.user"));
+            String[] tos = receiver.split(",");
+            for (String to : tos) {
+                email.addTo(to);
+            }
+            email.setSubject(alertEntity.getTitle());
+            email.setHtmlMsg(alertEntity.getContent());
+            email.send();
+            return true;
+        } catch (Exception e) {
+            Cat.logError(e);
+            return false;
+        }
+    }
+
+    @Override
+    public void initialize() throws InitializationException {
+        InputStream in = null;
+        InputStreamReader reader = null;
+        try {
+            in = new FileInputStream(new File(PROPERTIES_ALERT));
+            reader = new InputStreamReader(in, "UTF-8");
+            catProps.load(reader);
+            m_logger.info(String.format("initialing cat properties: %s", catProps));
+            authenticator = new Authenticator() {
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    String userName = catProps.getProperty("mail.user");
+                    String password = catProps.getProperty("mail.password");
+                    return new PasswordAuthentication(userName, password);
+                }
+            };
+            initialized = true;
+        } catch (IOException e) {
+            Cat.logError(e);
+        } finally {
+            if (null != in) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    Cat.logError(e);
+                }
+            }
+            if (null != reader) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    Cat.logError(e);
+                }
+            }
+        }
+    }
 }
